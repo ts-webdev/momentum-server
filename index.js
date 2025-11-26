1ea5cd64d11e044aa93e359335cc7585d981f7f6
ts-webdev <dev.tanvirmahtab@gmail.com>
2025-11-26 23:17:10 +0600

    added post event API

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.b6ihxc4.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.b6ihxc4.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,39 +24,64 @@ app.get("/", (req, res) => {
 
 async function run() {
   try {
-    
     await client.connect();
-    
+
     const db = client.db("momentumDB");
-    const eventCollections = db.collection("events")
+    const eventCollections = db.collection("events");
+    const blogCollections = db.collection("blogPosts");
 
-    // get all Events API
-    app.get("/events", async(req, res) =>{
-        const result = await eventCollections.find().toArray();
-        res.send(result)
-    })
-
-    // get latest Events api
-    app.get("/events/latest", async(req, res)=>{
-      const result = await eventCollections.find().sort({date: -1}).limit(3).toArray();
-        res.send(result)
-    })
-
-    // get a Event by id
-    app.get("/events/:id", async(req, res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await eventCollections.findOne(query);
+    // post a events api
+    app.post("/events", async(req, res)=>{
+      const newEvent = req.body;
+      const result = await eventCollections.insertOne(newEvent)
       res.send(result)
     })
 
+    // get all Events API
+    app.get("/events", async (req, res) => {
+      const result = await eventCollections.find().toArray();
+      res.send(result);
+    });
+
+    // get latest Events api
+    app.get("/events/latest", async (req, res) => {
+      const result = await eventCollections
+        .find()
+        .sort({ date: -1 })
+        .limit(3)
+        .toArray();
+      res.send(result);
+    });
+
+    // get a Event by id
+    app.get("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await eventCollections.findOne(query);
+      res.send(result);
+    });
+
+    // get all blog posts
+    app.get("/blogs", async (req, res) => {
+      const result = await blogCollections.find().toArray();
+      res.send(result);
+    });
+
+    // get latest blogs api
+    app.get("/blogs/latest", async (req, res) => {
+      const result = await blogCollections
+        .find()
+        .sort({ date: -1 })
+        .limit(3)
+        .toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
       "---------------------Pinged your deployment. You successfully connected to MongoDB!----------------------"
     );
   } finally {
-   
     // await client.close();
   }
 }
